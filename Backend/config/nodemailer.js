490eec89965f2e8cdae64cab8667038d849ce27e
abdullah-full-stack-dev <khannnabdullah7750@gmail.com>
2026-08-06 12:08:56 +0530

    updated file

--- Backend/config/nodemailer.js
+++ Backend/config/nodemailer.js
@@ -3,7 +3,7 @@ import 'dotenv/config'
 
 const transporter = nodemailer.createTransport({
     host: "smtp-relay.brevo.com",
-    port: 465,
+    port: 587,
     secure: true,
     auth: {
         user: process.env.SMTP_USER,
