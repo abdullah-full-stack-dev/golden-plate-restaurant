fd86fc88165c9bd1e2289ecacfad9e047117621d
abdullah-full-stack-dev <khannnabdullah7750@gmail.com>
2026-08-06 12:20:32 +0530

    updated files

--- Backend/controller/newsletterController.js
+++ Backend/controller/newsletterController.js
@@ -1,5 +1,6 @@
 import transporter from '../config/nodemailer.js';
 import newsLetterModel from "../models/newsletterModel.js";
+import axios from "axios";
 
 export const createSubscription = async (req, res) => {
     try {
@@ -19,6 +20,61 @@ export const createSubscription = async (req, res) => {
             email
         })
 
+        // Customer Mail
+        await axios.post(
+            "https://api.brevo.com/v3/smtp/email",
+            {
+                sender: {
+                    name: "Golden Plate Restaurant",
+                    email: process.env.SENDER_EMAIL,
+                },
+                to: [
+                    {
+                        email,
+                    },
+                ],
+                subject: "Subscribed Successfully",
+                htmlContent: `
+        <h2>Thank You!</h2>
+        <p>Hey ${email},</p>
+        <p>Thank you for subscribing to <b>Golden Plate Restaurant</b>.</p>
+      `,
+            },
+            {
+                headers: {
+                    "api-key": process.env.BREVO_API_KEY,
+                    "Content-Type": "application/json",
+                },
+            }
+        );
+
+
+        await axios.post(
+            "https://api.brevo.com/v3/smtp/email",
+            {
+                sender: {
+                    name: "Golden Plate Restaurant",
+                    email: process.env.SENDER_EMAIL,
+                },
+                to: [
+                    {
+                        email: process.env.SENDER_EMAIL,
+                    },
+                ],
+                subject: "New Subscription Received",
+                htmlContent: `
+                <h2>New Subscription</h2>
+                <p><b>Email:</b> ${email}</p>
+              `,
+            },
+            {
+                headers: {
+                    "api-key": process.env.BREVO_API_KEY,
+                    "Content-Type": "application/json",
+                },
+            }
+        );
+
         // transporter.verify((error, success) => {
         //     if (error) {
         //         console.log("SMTP ERROR:", error);
@@ -29,30 +85,30 @@ export const createSubscription = async (req, res) => {
 
 
         // sending mail to customer
-        const mailOptions = {
-            from: process.env.SENDER_EMAIL,
-            to: email,
-            subject: "Subscribed successfully.",
-            text: `Hey ${email}, Thank you for subscription.
-            - The Golden Plate Restaurant
-            `
-        }
+        // const mailOptions = {
+        //     from: process.env.SENDER_EMAIL,
+        //     to: email,
+        //     subject: "Subscribed successfully.",
+        //     text: `Hey ${email}, Thank you for subscription.
+        //     - The Golden Plate Restaurant
+        //     `
+        // }
 
         // sending mail to owner 
-        const mailOptions2 = {
-            from: process.env.SENDER_EMAIL,
-            to: process.env.SENDER_EMAIL,
-            subject: "New Subscription Received",
+        //     const mailOptions2 = {
+        //         from: process.env.SENDER_EMAIL,
+        //         to: process.env.SENDER_EMAIL,
+        //         subject: "New Subscription Received",
 
-            html: `
-        <h2>New Subscription</h2>
-        <p><b>Email:</b> ${email}</p>
-        
-    `
-        }
+        //         html: `
+        //     <h2>New Subscription</h2>
+        //     <p><b>Email:</b> ${email}</p>
 
-        await transporter.sendMail(mailOptions)
-        await transporter.sendMail(mailOptions2)
+        // `
+        //     }
+
+        //     await transporter.sendMail(mailOptions)
+        //     await transporter.sendMail(mailOptions2)
 
         res.send(
             {
